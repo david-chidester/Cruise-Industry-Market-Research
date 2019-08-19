4ce712e37f6b1020ffcf4b8602005e8c774fc2d6
David Chidester <31967957+dchid@users.noreply.github.com>
2019-08-19 14:42:32 -0700

    changed absolute path to reletive path

--- cruise_data.r
+++ cruise_data.r
@@ -1,2 +1,2 @@
 # reading in market reseach data from various cruise lines in north america
-northAmericaCruise <- read.csv(file.path("C:/Users/dchid/Desktop/Neurodiversity_in_the_Workplace_Data_Analysis/raw_data/northamericacruisedetaildata.csv"))
+northAmericaCruise <- read.csv(file.path("raw_data/northamericacruisedetaildata.csv"))
