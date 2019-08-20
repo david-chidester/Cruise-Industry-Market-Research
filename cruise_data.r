26a217b6775f8ec8620aff44cc515a266c053285
dchid <daviddan22@gmail.com>
2019-08-20 10:18:38 -0700

    Added finance data frames

--- cruise_data.r
+++ cruise_data.r
@@ -1,4 +1,4 @@
 # note, you'll need to be in this project's root directory for the data to load correctly
 # reading in market reseach data from various cruise lines in north america
 northAmericaCruise <- read.csv(file.path("raw_data/northamericacruisedetaildata.csv"), stringsAsFactors=FALSE)
-
+annualFinance <- read.csv(file.path("raw_data/Annual_Financial_Statistics.csv"))
