688a080fe9258f5349decdce3a61cfe1bdbe209c
David Chidester <31967957+dchid@users.noreply.github.com>
2019-08-20 14:32:15 -0700

    Corrected paths

--- cruise_data.r
+++ cruise_data.r
@@ -1,6 +1,7 @@
 # note, you'll need to be in this project's root directory for the data to load correctly
 # reading in market reseach data from various cruise lines in north america
-northAmericaCruise <- read.csv(file.path("raw_data/northamericacruisedetaildata.csv"), stringsAsFactors=FALSE)
+northAmericaCruise <- read.csv(file.path("raw_data/department_of_transportation/maritime_administration/
+northamericacruisedetaildata.csv"), stringsAsFactors=FALSE)
 
 # financial information about the cruise industry
 annualFinance <- read.csv(file.path("raw_data/Annual_Financial_Statistics.csv"))
