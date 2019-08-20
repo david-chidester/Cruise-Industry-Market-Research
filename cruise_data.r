b8757c334b695ae49cbce642c61243863418bcb1
dchid <daviddan22@gmail.com>
2019-08-20 09:54:21 -0700

    removed extranious data

--- cruise_data.r
+++ cruise_data.r
@@ -1,3 +1,4 @@
 # note, you'll need to be in this project's root directory for the data to load correctly
 # reading in market reseach data from various cruise lines in north america
 northAmericaCruise <- read.csv(file.path("raw_data/northamericacruisedetaildata.csv"), stringsAsFactors=FALSE)
+
