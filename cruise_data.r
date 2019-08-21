9c371340c205ca376ba7ca4ac161cb6aef3be03f
David Chidester <31967957+dchid@users.noreply.github.com>
2019-08-21 13:42:54 -0700

    fixed file read errors

--- cruise_data.r
+++ cruise_data.r
@@ -1,12 +1,9 @@
 # note, you'll need to be in this project's root directory for the data to load correctly
 # reading in market reseach data from various cruise lines in north america
-northAmericaCruise <- read.csv(file.path("raw_data/department_of_transportation/maritime_administration/
-northamericacruisedetaildata.csv"), stringsAsFactors=FALSE)
 
 # financial information about the cruise industry
 annualFinance <- read.csv(file.path("raw_data/Annual_Financial_Statistics.csv"))
 quarterlyFinance <- read.csv(file.path("raw_data/Quarterly_Financial_Statistics.csv"))
 
 #primary data set on ship maintainance is very large, so it's abridged
-shipMaintenance <- "raw_data/Ship_Maintainance_Abridged.csv"
-MaintenanceDF <- read.csv(file.path(shipMaintenance)
+maintenanceDF <- read.csv(file.path("raw_data/Ship_Maintenance_Abridged.csv"))
